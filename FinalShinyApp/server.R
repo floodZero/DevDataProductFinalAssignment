c3580d6ac5bffbbdf20901330c4536230a93efdb
myekim <myunghoonse@gmail.com>
2017-01-15 22:27:40 +0900

    Intermediate 2

--- FinalShinyApp/server.R
+++ FinalShinyApp/server.R
@@ -1,13 +1,14 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+options(shiny.reactlog = TRUE)
 
 shinyServer(function(input, output) {
   # Initialize data
   dataset <- reactive({
     data("mtcars")
     mtcars$am <- as.factor(mtcars$am)
-    levels(mtcars$am) <-c("Automatic", "Manual")
+    levels(mtcars$am) <- c("Automatic", "Manual")
     mtcars$cyl <- as.factor(mtcars$cyl)
     mtcars$vs <- as.factor(mtcars$vs)
     mtcars$gear <- as.factor(mtcars$gear)
@@ -20,74 +21,91 @@ shinyServer(function(input, output) {
   output$radioEDAFeature <- renderUI({
     dataMtcars <- dataset()
     
-    radioButtons("edaFeature", "Select Feature to examine",
-                 choices = names(dataMtcars)[grep("mpg", x= names(dataMtcars), invert = TRUE)],
-                 selected = "am"
-                 )
+    radioButtons(
+      "edaFeature",
+      "Select Feature to examine",
+      choices = names(dataMtcars)[grep("mpg", x = names(dataMtcars), invert = TRUE)],
+      selected = "am"
+    )
   })
   
   output$graphType <- renderUI({
-    selectedVar <-ifelse(is.null(input$edaFeature), "am", input$edaFeature)
+    selectedVar <-
+      ifelse(is.null(input$edaFeature), "am", input$edaFeature)
     
-    if (is.factor(dataset()[selectedVar][,1])) {
-      radioButtons("graphType", "Shape of Graphs", 
-                   choices = c("Violin", "Boxplot")
-      )
+    if (is.factor(dataset()[selectedVar][, 1])) {
+      radioButtons("graphType",
+                   "Shape of Graphs",
+                   choices = c("Violin", "Boxplot"))
     } else {
-      radioButtons("graphType", "Shape of Graphs", 
-                   choices = c("Dotplot")
-      )
+      radioButtons("graphType", "Shape of Graphs",
+                   choices = c("Dotplot"))
     }
   })
-
+  
   output$edaPlot <- renderPlot({
-    selectedGraphType <- ifelse(is.null(input$graphType), "Violin", input$graphType)
-
-    plotGeom <- switch(selectedGraphType,
-                       "Violin" = geom_violin(size=1),
-                       "Boxplot" = geom_boxplot(size=1),
-                       "Dotplot" = geom_dotplot()
+    selectedGraphType <-
+      ifelse(is.null(input$graphType), "Violin", input$graphType)
+    
+    plotGeom <- switch(
+      selectedGraphType,
+      "Violin" = geom_violin(size = 1),
+      "Boxplot" = geom_boxplot(size = 1),
+      "Dotplot" = geom_dotplot()
     )
-
-    xAxis <-ifelse(is.null(input$edaFeature), "am", input$edaFeature)
+    
+    xAxis <-
+      ifelse(is.null(input$edaFeature), "am", input$edaFeature)
     dataMtcars <- dataset()
-
-    edaplot <- ggplot(data=dataMtcars,aes_string(y="mpg",x=xAxis,fill=xAxis))
-    if (!is.factor(dataMtcars[xAxis][,1])) {
-      edaplot <- ggplot(data=dataMtcars,aes_string(y="mpg",x=xAxis))
+    
+    edaplot <-
+      ggplot(data = dataMtcars, aes_string(y = "mpg", x = xAxis, fill = xAxis))
+    if (!is.factor(dataMtcars[xAxis][, 1])) {
+      edaplot <- ggplot(data = dataMtcars, aes_string(y = "mpg", x = xAxis))
     }
     edaplot <- edaplot +
       plotGeom +
       xlab(xAxis) +
       ylab("Mpg")
-
+    
     edaplot
   })
-
+  
   ## Part of Tab 2
   output$chkbxModelFeature <- renderUI({
     dataMtcars <- dataset()
-    checkboxGroupInput("chkModelFeature", "Select Feature to examine",
-                 choices = names(dataMtcars)[grep("mpg", x= names(dataMtcars), invert = TRUE)],
-                 selected = "am"
+    checkboxGroupInput(
+      "chkModelFeature",
+      "Select Feature to examine",
+      choices = names(dataMtcars)[grep("mpg", x = names(dataMtcars), invert = TRUE)],
+      selected = "am"
     )
   })
   
   model <- reactive({
     fit <- NULL
     dataMtcars <- dataset()
-    if (input$fitAutomation == "Manual") {
-      factors <- ifelse(is.null(input$chkModelFeature), c("am"), input$chkModelFeature)
-      formula <- paste("mpg~", paste(factors, collapse="+")) 
+    selectedVars <- input$chkModelFeature
+    selectedAutomationType <- input$fitAutomation
+    
+    if (selectedAutomationType == "Manual") {
+      factors <- if (is.null(selectedVars)) {
+        c("am")
+      } else {
+        selectedVars
+      }
+      formula <- paste("mpg ~ ", paste(factors, collapse = " + "))
       fit <- lm(formula, data = dataMtcars)
     }
-    else if(input$fitAutomation == "Automated") {
-      fit = step(lm(mpg ~ ., data = dataMtcars),trace=0,steps=100)
+    else if (selectedAutomationType == "Automated") {
+      fit = step(lm(mpg ~ ., data = dataMtcars),
+                 trace = 0,
+                 steps = 100)
     }
     
     fit
   })
-
+  
   output$modelSummary <- renderPrint({
     input$buildButton
     
@@ -102,7 +120,7 @@ shinyServer(function(input, output) {
     
     isolate({
       fit <- model()
-      par(mfrow = c(2,2))
+      par(mfrow = c(2, 2))
       plot(fit)
     })
   })
--- FinalShinyApp/ui.R
+++ FinalShinyApp/ui.R
@@ -2,33 +2,36 @@ library(shiny)
 
 shinyUI(fluidPage(
   titlePanel("mtcars data correlation between MPG and other variables"),
-  tabsetPanel(tabPanel("EDA",
-                       fluidPage(
-                         br(),
-                         sidebarLayout(sidebarPanel(
-                           uiOutput('radioEDAFeature'),
-                           uiOutput('graphType')
-                         ),
-                         mainPanel(plotOutput("edaPlot")))
-                       )),
-              tabPanel("Regression Analysis",
-                       fluidPage(
-                         br(),
-                         sidebarLayout(sidebarPanel(
-                           radioButtons("fitAutomation", "How to  select model",
-                                        choices = c("Manual", "Automated")
-                           ),
-                           # uiOutput('chkbxModelFeature'),
-                           checkboxGroupInput("chkModelFeature", "Select Feature to examine",
-                                    choices = c("am", "cyl", "disp"),
-                                    selected = "am"
-                                    ),
-                           actionButton("buildButton", "Build")
-                         ),
-                         mainPanel(
-                           h3("Model"),
-                           verbatimTextOutput("modelSummary"),
-                           h3("Diagnostics"),
-                           plotOutput("diagnosticsPlot")))
-                       )))
+  tabsetPanel(
+    tabPanel("EDA",
+             fluidPage(
+               br(),
+               sidebarLayout(sidebarPanel(
+                 uiOutput('radioEDAFeature'),
+                 uiOutput('graphType')
+               ),
+               mainPanel(plotOutput("edaPlot")))
+             )),
+    tabPanel("Regression Analysis",
+             fluidPage(
+               br(),
+               sidebarLayout(
+                 sidebarPanel(
+                   radioButtons(
+                     "fitAutomation",
+                     "How to  select model",
+                     choices = c("Manual", "Automated")
+                   ),
+                   uiOutput('chkbxModelFeature'),
+                   actionButton("buildButton", "Build")
+                 ),
+                 mainPanel(
+                   h3("Model"),
+                   verbatimTextOutput("modelSummary"),
+                   h3("Diagnostics"),
+                   plotOutput("diagnosticsPlot")
+                 )
+               )
+             ))
+  )
 ))
